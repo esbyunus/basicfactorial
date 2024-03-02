e95e7737206f32fc0e70685108a4eb6775756e3b
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-02 20:44:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number whose factorial you want to calculate: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            static int FactorialCalculator( int n )
+                if (n == 0)
+                    return 1;
+                else 
+                    return n * FactorialCalculator(n - 1);
+        }
+
+            int answer = FactorialCalculator(number);
+            Console.WriteLine($"factorial: {answer}");
+        }
+    }
+}
